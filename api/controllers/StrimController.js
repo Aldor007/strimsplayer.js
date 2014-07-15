3c42467b1843a2ec2882bc99a4f0b7c1145c4f3e
Marcin Kaciuba <aldor007@gmail.com>
2014-07-15 18:03:43 +0200

    testy

--- api/controllers/StrimController.js
+++ api/controllers/StrimController.js
@@ -8,7 +8,7 @@ var request = require('request');
 
 module.exports = {
     get: function (req, res) {
-        var name = req.param('name').toLowerCase();
+        var name = req.param('name');
         if (name) {
             res.redirect('/#!s/'+ name);
         } else {
@@ -22,7 +22,7 @@ module.exports = {
         Strim.find().sort('name ASC').exec(function (err, strims) {
             if (err) {
                 sails.log.error('StrimController/list error=' + JSON.stringify(err));
-                return res.send(500);
+                return res.json({message: 'DB error'}, err.status || 500);
             } else {
                 res.json(strims);
             }
@@ -36,7 +36,7 @@ module.exports = {
             Strim.create({name: strimName}).exec(function(err, strim){
                 if(err) {
                     sails.log.error('StrimyController/add error', err);
-                    return res.json({status: 'error', message: 'Strim istnieje!'}, err.status);
+                    return res.json({status:  err.status, message: 'Strim istnieje!'}, err.status);
                 } else {
                     console.info('StrimyController/add 200 added strim  strimName=' + strimName);
                     request({url: 'http://' + sails.config.cliapi.HostAndPORT + '/songscollectorfromstrim/'+ strimName}, function(errorUpdate, response, body) {
@@ -45,10 +45,9 @@ module.exports = {
                         } else {
                             sails.log.info('StrimController/add added songs', body);
                         }
-
                              
                     });
-                    res.json({status: 'ok', message: 'Strim ' + strim + 'dodany'},200);
+                    res.json({status: 200, message: 'Strim ' + strimName + 'dodany'}, 200);
                 }
 
             });
@@ -61,20 +60,20 @@ module.exports = {
                     followRedirect: false,
                     timeout: 500
                 }, function (error, response, body) {
-                  if (!error && response.statusCode == 200) {
+                  if (!error && response.statusCode === 200) {
                         if (body.indexOf('<p>Nie znaleziono treści.</p>') === -1) {
                             saveStrim(strimName);
                         }
                         else {
                             errorRes.message = "Nie znaleziono filmów na "+ strimName;
                             console.info('StrimController/add 404 Not propert strim  strimName=' + strimName);
-                            res.json(errorRes, 404)
+                            res.json(errorRes, 404);
                         }
                     } else {
                     
                             errorRes.message = "Nie znaleziono strimu o nazwie "+ strimName;
                             console.info('StrimController/add 404 Not Found strim with strimName=' + strimName);
-                            res.json(errorRes, 404)
+                            res.json(errorRes, 404);
                     }
 
                 });
