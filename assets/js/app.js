5d179c2bf78e79128fadc230f26b6ed971abe31c
strah <mstrasz@gmail.com>
2014-08-17 19:11:02 +0100

    test runner

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,35 +14,37 @@ StrimsPlayerApi.ADD_STRIM = '/api/strims/add/';
 
 var saveApply = function ($scope, data) {
     try {
-        if (!$scope.$$phase) {
-            $scope.$apply(data);
-        }
-    } catch (err) {
-        console.log(err);
+     if (!$scope.$$phase) {
+        $scope.$apply(data);
     }
+        } catch (err) {
+     console.log(err);
+     }
 };
 
 
 
 
 var app = angular.module('app', [
-        'ui.bootstrap',
+        'ui.bootstrap', 
         'ngRoute',
         'infinite-scroll'
         ]).config(['$interpolateProvider', '$routeProvider', function($interpolateProvider, $routeProvider){
-            $interpolateProvider.startSymbol('[[').endSymbol(']]');
-            $routeProvider.
-                when('/s/:strim', {
-                    templateUrl: 'templates/strims/songs.html',
-                    controller: 'PlayerCtrl'
-                }).
-                when('/strim/:strim', {
-                    templateUrl: 'templates/strims/songs.html',
-                    controller: 'PlayerCtrl'
-                }).
-                otherwise({
-                    templateUrl: 'templates/strims/songs.html',
-                    controller: 'PlayerCtrl'
-                });
-        }
+        $interpolateProvider.startSymbol('[[').endSymbol(']]');
+        $routeProvider.
+          when('/s/:strim', {
+                templateUrl: 'templates/strims/songs.html',
+              controller: 'PlayerCtrl'
+            }).
+          when('/strim/:strim', {
+            templateUrl: 'templates/strims/songs.html',
+              controller: 'PlayerCtrl'
+            }).
+          otherwise({
+            templateUrl: 'templates/strims/songs.html',
+          controller: 'PlayerCtrl'
+        });
+    }
 ]);
+
+
